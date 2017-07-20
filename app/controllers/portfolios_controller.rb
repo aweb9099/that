193607c6acb5c99ca00acdee191b1c0e2f43f24b
Jordan Hudgens <aweb9099@gmail.com>
2017-07-20 14:56:47 +0000

    drag and drop working

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,7 +6,15 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.by_position
   end
-
+  
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+    
+    render nothing: true
+  end
+  
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
